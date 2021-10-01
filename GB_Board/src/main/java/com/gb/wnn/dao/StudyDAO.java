1b12b8e50a3a78313d0a5bd5889f8299aa437986
suhyunshim <lisa9963suhyun@gmail.com>
2021-10-01 17:11:45 +0900

    mapper, dao 연동

--- GB_Board/src/main/java/com/gb/wnn/dao/StudyDAO.java
+++ GB_Board/src/main/java/com/gb/wnn/dao/StudyDAO.java
@@ -1,19 +1,15 @@
 package com.gb.wnn.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
-
-import org.springframework.stereotype.Repository;
 import com.gb.wnn.vo.StudyVO;
 
 //mapper interface 참조
 public interface StudyDAO {
 	ArrayList <StudyVO> getAllStudy();
-	void insertSawon(StudyVO sawonVO);
-	StudyVO getTelinfo(StudyVO sawonVO);
-	void updateTelinfo(StudyVO sawonVO);
-	void deleteTelinfo(StudyVO sawonVO);
+	void insertStudy(StudyVO studyVO); //스터디 입력
+	StudyVO getStudy_region(StudyVO studyVO); //지역으로 스터디 찾기
+	StudyVO getStudy_tag(StudyVO studyVO); //태그로 스터디 찾기
+	StudyVO getStudy_name(StudyVO studyVO); //이름으로 스터디 찾기
+		
+	void deleteStudy(StudyVO studyVO); //스터디를 삭제한다.
 }
