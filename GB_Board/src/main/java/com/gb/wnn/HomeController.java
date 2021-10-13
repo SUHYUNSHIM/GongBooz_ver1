be296d0803ca017d3bb889206de5b78039d9e1ce
suhyunshim <lisa9963suhyun@gmail.com>
2021-10-13 23:50:50 +0900

    다시 로드

---
+++ GB_Board/src/main/java/com/gb/wnn/HomeController.java
@@ -0,0 +1,90 @@
+package com.gb.wnn;
+
+import java.sql.SQLException;
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.gb.wnn.service.StudyService;
+import com.gb.wnn.vo.StudyVO;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	@Resource(name="StudyService")
+	private StudyService studyService;
+
+	//전체 스터디 목록 조회
+	@RequestMapping(value="/studyAll.do")
+	public String studyAll(Model model) throws Exception{
+		ArrayList <StudyVO> alist = studyService.getAllStudy();
+		model.addAttribute("alist",alist);
+		return "studyAll"; //studyAll.jsp에서 전부 출력
+	}
+	
+	@RequestMapping(value="/studyInsertForm.do")
+	public String studyInsertForm(@ModelAttribute("studyVO") StudyVO studyVO, Model model) throws Exception{
+		return "studyInsertForm";
+	}
+	
+	@RequestMapping(value="/studyInsert.do")
+	public String studyInsert(@ModelAttribute("studyVO") StudyVO studyVO, Model model) throws Exception {
+		studyService.insertStudy(studyVO);		
+		studyService.getAllStudy();
+	
+		return "redirect:/studyAll.do";
+	}
+	
+	@RequestMapping(value="/search-controller.do", method= RequestMethod.POST)
+	public String studySearch(@ModelAttribute("studyVO") StudyVO studyVO,Model model, HttpServletRequest req) throws Exception {
+		String filter = req.getParameter("filter");
+		String search = req.getParameter("search");	
+		
+		String whatSearched = filter+"/"+search;
+		model.addAttribute("whatSearched", whatSearched);
+		
+		if(filter.equals("study_name")) {
+			studyVO.setStudy_name(search);
+			ArrayList <StudyVO> svo_name = studyService.getStudy_name(studyVO);
+			model.addAttribute("alist",svo_name);
+			req.setAttribute("alist", svo_name);
+						
+			return "studyAll";
+		}
+		else if(filter.equals("study_tag")) {
+			studyVO.setStudy_tag(search);
+			ArrayList <StudyVO> svo_tag = studyService.getStudy_tag(studyVO);
+			model.addAttribute("alist",svo_tag);
+			req.setAttribute("alist", svo_tag);
+			return "studyAll";
+		}
+		else if(filter.equals("region")) {
+			studyVO.setRegion(search);
+			ArrayList <StudyVO> svo_region = studyService.getStudy_region(studyVO);
+			model.addAttribute("alist",svo_region);
+			req.setAttribute("alist", svo_region);
+			return "studyAll";
+		}
+		studyService.getAllStudy();	
+		return "redirect:/studyAll";
+		
+	}
+	
+	
+}
---
+++ GB_Board/src/main/webapp/index.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>스터디 모집 메인 게시판</title>
+</head>
+<body>
+
+<div id="study_list">		
+	<script>
+		location.href="studyAll.do"  //기존의 mybatis project에서 한 방법*/	
+	</script>	
+</div>
+
+</body>
+</html>
